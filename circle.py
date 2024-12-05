152cad5431b109d3c774951cdf146257b0ff1464
13-arch <Iron_Mike@tutamail.com>
2024-12-05 18:37:38 +0300

    laba#7

--- circle.py
+++ circle.py
@@ -1,17 +1,16 @@
-def rad(r):
-    S=3.14*r**2
+def rad(r):#задаем функцию
+    S=3.14*r**2#задаем значения функции
     return S
-
 def dlin(r):
     C=2*3.14*r
     return C
   
 
 chislo=int(input('введите число: ')) 
-result1=rad(chislo) 
+result1=rad(chislo)#вызов функции 
 result2=dlin(chislo)
 print('длина окружности равна:',result2)
-print('площадь круга равна:',result1)
+print('площадь круга равна:',result1)#вывод функии
 
 
 
